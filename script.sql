e9eae660b37e35c48affa657b89efddfd83e989c
JPZU <juanpablozp@icloud.com>
2024-11-17 18:21:28 -0500

    configuracion inicial completa -CONEXION BASE DE DATOS + -Configuracion inicial

--- script.sql
+++ script.sql
@@ -28,16 +28,16 @@ CREATE TABLE productos(
     estado TINYINT,
 	CONSTRAINT fk_categoria_id_categoria FOREIGN KEY (id_categoria) 
     REFERENCES categorias(id_categoria) 
-    ON UPDATE CASCADE 
-    ON DELETE RESTRICT
+    ON UPDATE NO ACTION 
+    ON DELETE NO ACTION
 );
 
 CREATE TABLE clientes(
 	id VARCHAR(20) PRIMARY KEY,
     nombre VARCHAR(40),
     apellidos VARCHAR(100),
-    celular VARCHAR(150),
-	direccion DECIMAL,
+    celular DECIMAL,
+	direccion VARCHAR(80),
     correo_electronico VARCHAR(70)
 );
 
@@ -50,24 +50,25 @@ CREATE TABLE compras(
     estado CHAR(1),
 	CONSTRAINT fk_clientes_id_cliente FOREIGN KEY (id_cliente) 
     REFERENCES clientes(id) 
-    ON UPDATE CASCADE 
-    ON DELETE RESTRICT
+    ON UPDATE NO ACTION 
+    ON DELETE NO ACTION
 );
 
 CREATE TABLE compras_productos(
-	id_compra INT PRIMARY KEY,
+	id_compra INT,
     id_producto INT,
 	cantidad INT, 
     total DECIMAL(16,2),
     estado TINYINT,
+    PRIMARY KEY (id_compra, id_producto),
 	CONSTRAINT fk_compras_id_compra FOREIGN KEY (id_compra) 
     REFERENCES compras(id_compra) 
-    ON UPDATE CASCADE 
-    ON DELETE RESTRICT,
+    ON UPDATE NO ACTION 
+    ON DELETE NO ACTION,
 	CONSTRAINT fk_productos_id_producto FOREIGN KEY (id_producto) 
     REFERENCES productos(id_producto) 
-    ON UPDATE CASCADE 
-    ON DELETE RESTRICT
+    ON UPDATE NO ACTION 
+    ON DELETE NO ACTION
 );
 
 
